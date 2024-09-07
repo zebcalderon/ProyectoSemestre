6134ea8067cbd2d24bd0c501e4b2c9b14a2a0148
D4v1d003 <davidvalenzuelamaldonado@gmail.com>
2024-09-07 17:15:19 -0400

    calculadora-macros-validacion seleccion

--- src/app/paginas/calculadora-macros/calculadora-macros.page.ts
+++ src/app/paginas/calculadora-macros/calculadora-macros.page.ts
@@ -7,14 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraMacrosPage implements OnInit {
   selectedOption: string | null = null;
+  selectedSex: string | null = null;
+  selectedActivityLevel: string | null = null;
 
   constructor() { }
 
   ngOnInit() { }
 
-  // Maneja el cambio de opción
+  // Maneja el cambio de opción en el acordeón "Selecciona tu objetivo"
   onOptionChange(value: string) {
     this.selectedOption = value;
+    // Aquí puedes agregar lógica adicional si es necesario
+  }
+
+  // Maneja el cambio de opción en el acordeón "Sexo"
+  onSexChange(value: string) {
+    this.selectedSex = value;
+    // Aquí puedes agregar lógica adicional si es necesario
+  }
+
+  // Maneja el cambio de opción en el acordeón "Nivel de Actividad Física"
+  onActivityLevelChange(value: string) {
+    this.selectedActivityLevel = value;
     // Aquí puedes agregar lógica adicional si es necesario
   }
 }
