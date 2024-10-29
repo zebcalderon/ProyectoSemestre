72c2ef14de8edd8b6f0fb656171fb1fc31c90646
zebcalderon <seb.calderon@duocuc.cl>
2024-10-29 12:05:50 -0300

    cambios pendientes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCc52X8zmTRUiu6vjIAvxdd3AMTbWMOO4g',
+    authDomain: 'proyectosemestre-6d3c0.firebaseapp.com',
+    projectId: 'proyectosemestre-6d3c0',
+    storageBucket: 'proyectosemestre-6d3c0.appspot.com',
+    messagingSenderId: '47390431302',
+    appId: '1:47390431302:android:47e5a5e6e52fd18c411247',
+  }
 };
 
 /*
