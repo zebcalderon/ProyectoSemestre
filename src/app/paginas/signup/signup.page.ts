37f2122103fd72dfa97b49d9723a4f894b2459e6
zebcalderon <seb.calderon@duocuc.cl>
2024-10-29 14:50:58 -0300

    gitignore

--- src/app/paginas/signup/signup.page.ts
+++ src/app/paginas/signup/signup.page.ts
@@ -8,11 +8,11 @@ import { AuthenticationService } from 'src/app/authentication.service';
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
+
 export class SignupPage implements OnInit {
   correo: string = '';
   nombre: string = '';
   password: string = '';
-  errors: any = {};
   
   regForm: FormGroup
 
@@ -45,37 +45,4 @@ export class SignupPage implements OnInit {
     }
   }
 
-  validateFields() {
-    this.errors = {};
-
-    if (!this.correo.includes('@') || !this.correo.includes('.')) {
-      this.errors.correoError = 'Correo electronico inválido';
-    }
-
-    if (this.nombre.length < 3 || this.nombre.length > 30) {
-      this.errors.nombreError = 'El nombre de usuario debe tener entre 3 y 30 caracteres';
-    }
-
-    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
-    if (!passwordPattern.test(this.password)) {
-      this.errors.passwordError = 'La contraseña debe tener entre 8 y 20 caracteres, incluir mayúsculas, minúsculas, números y símbolos';
-    }
-
-    return this.errors;
-  }
-
-  onRegister() {
-    if (!this.nombre || !this.correo || !this.password) {
-      this.errors = 'Todos los campos son obligatorios.';
-      return;
-    }
-
-    const usuario = {
-      nombre:this.nombre,
-      correo:this.correo,
-      password:this.password
-    }
-    localStorage.setItem("usuario",JSON.stringify(usuario))
-    console.log("Registro exitoso")
-  }
 }
