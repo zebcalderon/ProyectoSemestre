3377a5457ce2f6c84f8c36b18e0fe6acbf4bdb85
D4v1d003 <davidvalenzuelamaldonado@gmail.com>
2024-12-06 12:30:10 -0300

    camara listo

--- diet_app/src/environments/environment.prod.ts
+++ diet_app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCc52X8zmTRUiu6vjIAvxdd3AMTbWMOO4g',
+    authDomain: 'proyectosemestre-6d3c0',
+    projectId: 'proyectosemestre-6d3c0.firebaseapp.com',
+    storageBucket: 'proyectosemestre-6d3c0.appspot.com',
+    messagingSenderId: '47390431302',
+    appId: '1:47390431302:android:47e5a5e6e52fd18c411247',
+  }
 };
